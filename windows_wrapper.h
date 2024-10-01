d295f1f77eee7c902c661687163b0dac38558ac7
RobertEngen <rnengen@gmail.com>
2024-09-30 17:19:35 -0700

    Create windows_wrapper.h

---
+++ windows_wrapper.h
@@ -0,0 +1,6 @@
+// windows_wrapper.h
+#include "C:/ta-lib/c/include/ta_abstract.h"
+#include "C:/ta-lib/c/include/ta_common.h"
+#include "C:/ta-lib/c/include/ta_defs.h"
+#include "C:/ta-lib/c/include/ta_func.h"
+#include "C:/ta-lib/c/include/ta_libc.h"
